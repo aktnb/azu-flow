bf9a7778de88d8a7c10dd8df95226e20cfbb3389
aktnb <70847394+aktnb@users.noreply.github.com>
2026-07-01 00:36:22 +0900

    feat(api): serialize enums as strings in JSON responses

--- backend/AzuFlow.Api/Program.cs
+++ backend/AzuFlow.Api/Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(o => o.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter()));
 builder.Services.AddOpenApi();
 builder.Services.AddAzuFlowAzure(builder.Configuration);
 
